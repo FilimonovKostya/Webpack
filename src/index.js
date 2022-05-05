f774e0eaec9936269f418505928372e7a0f5f9c5
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-05-05 15:57:24 +0400

    work with xml loaders

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {Post} from "./Post";
 import "./style/styles.css"
 import json from './assets/json'
 import Logo from './assets/webpack-logo.png'
+import xml from './assets/data.xml'
 
 const post = new Post('Webpack post title', Logo)
 
@@ -9,3 +10,5 @@ console.log('Post to  string', post.toString())
 console.log('Title upperCase', post.upperCaseTitle())
 
 console.log('JSON --->', json)
+
+console.log('xml ', xml)
